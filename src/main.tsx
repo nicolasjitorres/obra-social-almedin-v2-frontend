2a2cc0ff16be5af117d0259266c14313bfbf7ea2
Nicolas Juan Ignacio Torres <nicoignacio123@gmail.com>
2026-02-20 11:32:38 -0300

    fix: fixed sintaxis in project

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { QueryClient } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const savedTheme = localStorage.getItem('almedin-theme') ?? 'light'
 document.documentElement.setAttribute('data-theme', savedTheme === 'dark' ? 'dark' : '')
@@ -17,6 +17,7 @@ const queryClient = new QueryClient({
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}></QueryClientProvider>
     <App />
   </StrictMode>,
 )
